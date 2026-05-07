32fe100f3bb51ef4a7b03badb9c495f8dee317e8
zub <ars1for1work@gmail.com>
2026-05-07 11:56:33 +0300

    fix: delete comments in commands.hpp

--- zubarev.arsenii/T1/commands.hpp
+++ zubarev.arsenii/T1/commands.hpp
@@ -9,13 +9,13 @@ namespace zubarev
 {
   void cmd_note(std::istream& in, std::ostream&, NoteBook& notebook);
   void cmd_line(std::istream& in, std::ostream&, NoteBook& notebook);
-  void cmd_show(std::istream& in, std::ostream& out, NoteBook& notebook); //
+  void cmd_show(std::istream& in, std::ostream& out, NoteBook& notebook);
   void cmd_drop(std::istream& in, std::ostream&, NoteBook& notebook);
 
   void cmd_link(std::istream& in, std::ostream&, NoteBook& notebook);
   void cmd_halt(std::istream& in, std::ostream&, NoteBook& notebook);
-  void cmd_mind(std::istream& in, std::ostream& out, NoteBook& notebook);    //
-  void cmd_expired(std::istream& in, std::ostream& out, NoteBook& notebook); //
+  void cmd_mind(std::istream& in, std::ostream& out, NoteBook& notebook);
+  void cmd_expired(std::istream& in, std::ostream& out, NoteBook& notebook);
   void cmd_refresh(std::istream& in, std::ostream&, NoteBook& notebook);
 }
 
