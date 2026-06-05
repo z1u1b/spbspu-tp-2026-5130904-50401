6d5f81168118914b443d284edc67e93c4ee78e0f
zub <ars1for1work@gmail.com>
2026-06-05 12:17:22 +0300

    feat(commands.cpp): add min command to find minimum number of vertices or area among polygons

--- zubarev.arsenii/T3/commands.cpp
+++ zubarev.arsenii/T3/commands.cpp
@@ -89,8 +89,33 @@ namespace zubarev
       os << std::fixed << std::setprecision(1) << *std::max_element(areas.begin(), areas.end()) << '\n';
     }
   }
-  // void min(std::istream& is, std::ostream& os, const std::vector< Polygon >& polygons)
-  // {}
+  void min(std::istream& is, std::ostream& os, const std::vector< Polygon >& polygons)
+  {
+    if (polygons.size() < 1) {
+      throw std::runtime_error("<INVALID COMMAND>");
+    }
+
+    std::string parameter = "";
+    is >> parameter;
+    if (!is) {
+      if (is.eof()) {
+        is.clear();
+        is.setstate(std::ios_base::eofbit);
+      } else {
+        is.clear();
+      }
+      throw std::runtime_error("<INVALID COMMAND>");
+    }
+    if (parameter == "VERTEXES") {
+      std::vector< size_t > count_vert;
+      std::transform(polygons.begin(), polygons.end(), std::back_inserter(count_vert), CountVert{});
+      os << *std::min_element(count_vert.begin(), count_vert.end()) << '\n';
+    } else if (parameter == "AREA") {
+      std::vector< double > areas;
+      std::transform(polygons.begin(), polygons.end(), std::back_inserter(areas), AreaCount{});
+      os << std::fixed << std::setprecision(1) << *std::min_element(areas.begin(), areas.end()) << '\n';
+    }
+  }
   // void count(std::istream& is, std::ostream& os, const std::vector< Polygon >& polygons)
   // {}
   // void maxseq(std::istream& is, std::ostream& os, const std::vector< Polygon >& polygons)
